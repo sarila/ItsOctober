307df729ef9f6a40e1ea93160f81bfdfc62de814
nikhiltandukar <91746348+nikhiltandukar@users.noreply.github.com>
2021-10-05 12:42:13 +0545

    law

---
+++ lawfirm/prac-detail.php
@@ -0,0 +1,63 @@
+<?php
+include('include/header.php');
+?>
+<div class="heading_container heading_center ">
+    <h2 class="">
+        <span class="dot_design"></span>Taxation & Accounts
+    </h2>
+</div>
+<section class="prac-detail">
+    <div class="container">
+        <div class="top-img">
+            <img src="assets/img/practice-detail/tax.jpg">
+        </div>
+        <div class="main-part">
+            <div class="row">
+
+                <div class="col-lg-6"><img src="assets/img/practice-detail/tax.jpg"></div>
+                <div class="col-lg-6">
+                    <h5> Our Services Including</h5>
+                    <ul>
+                        <li>Commercial and civil litigation</li>
+                        <li>Business formation and advice</li>
+                        <li>Due diligence</li>
+                        <li>Financial investment counseling</li>
+                        <li>Contract review and development</li>
+                        <li>Breach of contract</li>
+                        <li>Buy and sell agreements</li>
+                        <li>Shareholder disputes</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="second-part">
+            <h5>
+                Helping Business Owners Move Forward</h5>
+            <p> Wize Law Services works closely with business owners to ensure businesses run smoothly. We often
+                negotiate
+                and draft contracts and buy-and-sell agreements, assist with corporate compliance measures and tax
+                filing,
+                and represent businesses in shareholder disputes and commercial litigation cases. Our attorneys are
+                seasoned
+                negotiators and litigators, and know how to best represent your financial interests, successfully
+                resolving
+                your issues in the boardroom or the courtroom.</p>
+        </div>
+        <div class="second-part">
+            <h5>
+                Helping Business Owners Move Forward</h5>
+            <p> Wize Law Services works closely with business owners to ensure businesses run smoothly. We often
+                negotiate
+                and draft contracts and buy-and-sell agreements, assist with corporate compliance measures and tax
+                filing,
+                and represent businesses in shareholder disputes and commercial litigation cases. Our attorneys are
+                seasoned
+                negotiators and litigators, and know how to best represent your financial interests, successfully
+                resolving
+                your issues in the boardroom or the courtroom.</p>
+        </div>
+    </div>
+</section>
+<?php
+include('include/footer.php');
+?>
