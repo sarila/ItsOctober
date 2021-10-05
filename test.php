4456cd8db5600de3f723aaab62a00711cd50554f
jonishilalkar <91776936+jonishilalkar@users.noreply.github.com>
2021-10-05 15:45:58 +0545

    Add files via upload

---
+++ test.php
@@ -0,0 +1,84 @@
+<?php
+function sendIcalEvent($from_name, $from_address, $to_name, $to_address, $startTime, $endTime, $subject, $description, $location)
+{
+    $domain = 'askmeconsult.com';
+
+    //Create Email Headers
+    $mime_boundary = "----Meeting Booking----".MD5(TIME());
+
+    $headers = "From: ".$from_name." <".$from_address.">\n";
+    $headers .= "Reply-To: ".$from_name." <".$from_address.">\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-Type: multipart/alternative; boundary=\"$mime_boundary\"\n";
+    $headers .= "Content-class: urn:content-classes:calendarmessage\n";
+
+    //Create Email Body (HTML)
+    $message = "--$mime_boundary\r\n";
+    $message .= "Content-Type: text/html; charset=UTF-8\n";
+    $message .= "Content-Transfer-Encoding: 8bit\n\n";
+    $message .= "<html>\n";
+    $message .= "<body>\n";
+    $message .= '<p>Dear '.$to_name.',</p>';
+    $message .= '<p>'.$description.'</p>';
+    $message .= "</body>\n";
+    $message .= "</html>\n";
+    $message .= "--$mime_boundary\r\n";
+
+    $ical = 'BEGIN:VCALENDAR' . "\r\n" .
+    'PRODID:-//Microsoft Corporation//Outlook 10.0 MIMEDIR//EN' . "\r\n" .
+    'VERSION:2.0' . "\r\n" .
+    'METHOD:REQUEST' . "\r\n" .
+    'BEGIN:VTIMEZONE' . "\r\n" .
+    'TZID:Eastern Time' . "\r\n" .
+    'BEGIN:STANDARD' . "\r\n" .
+    'RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=1SU;BYMONTH=11' . "\r\n" .
+    'TZOFFSETFROM:-0400' . "\r\n" .
+    'TZOFFSETTO:-0500' . "\r\n" .
+    'TZNAME:EST' . "\r\n" .
+    'END:STANDARD' . "\r\n" .
+    'BEGIN:DAYLIGHT' . "\r\n" .
+    'RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=2SU;BYMONTH=3' . "\r\n" .
+    'TZOFFSETFROM:-0500' . "\r\n" .
+    'TZOFFSETTO:-0400' . "\r\n" .
+    'TZNAME:EDST' . "\r\n" .
+    'END:DAYLIGHT' . "\r\n" .
+    'END:VTIMEZONE' . "\r\n" .  
+    'BEGIN:VEVENT' . "\r\n" .
+    'ORGANIZER;CN="'.$from_name.'":MAILTO:'.$from_address. "\r\n" .
+    'ATTENDEE;CN="'.$to_name.'";ROLE=REQ-PARTICIPANT;RSVP=TRUE:MAILTO:'.$to_address. "\r\n" .
+    'LAST-MODIFIED:' . date("Ymd\TGis") . "\r\n" .
+    'UID:'.date("Ymd\TGis", strtotime($startTime)).rand()."@".$domain."\r\n" .
+    'DTSTAMP:'.date("Ymd\TGis"). "\r\n" .
+    'DTSTART;TZID="Eastern Time":'.date("Ymd\THis", strtotime($startTime)). "\r\n" .
+    'DTEND;TZID="Eastern Time":'.date("Ymd\THis", strtotime($endTime)). "\r\n" .
+    'TRANSP:OPAQUE'. "\r\n" .
+    'SEQUENCE:1'. "\r\n" .
+    'SUMMARY:' . $subject . "\r\n" .
+    'LOCATION:' . $location . "\r\n" .
+    'CLASS:PUBLIC'. "\r\n" .
+    'PRIORITY:5'. "\r\n" .
+    'BEGIN:VALARM' . "\r\n" .
+    'TRIGGER:-PT15M' . "\r\n" .
+    'ACTION:DISPLAY' . "\r\n" .
+    'DESCRIPTION:Reminder' . "\r\n" .
+    'END:VALARM' . "\r\n" .
+    'END:VEVENT'. "\r\n" .
+    'END:VCALENDAR'. "\r\n";
+    $message .= 'Content-Type: text/calendar;name="meeting.ics";method=REQUEST'."\n";
+    $message .= "Content-Transfer-Encoding: 8bit\n\n";
+    $message .= $ical;
+
+    $mailsent = mail($to_address, $subject, $message, $headers);
+
+    return ($mailsent)?(true):(false);
+}
+$from_name = "CRM - AskMe Management P. Ltd.";
+$from_address = "noreply@askmeconsult.com";        
+$to_name = "test";        
+$to_address = "suman@vertexsolution.com.np";        
+$startTime = "2019-07-26 18:00:00";        
+$endTime = "2019-07-26 19:00:00";        
+$subject = "My Test Subject";        
+$description = "My Awesome Description";        
+$location = "Joe's House";
+//sendIcalEvent($from_name, $from_address, $to_name, $to_address, $startTime, $endTime, $subject, $description, $location);
